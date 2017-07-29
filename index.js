42651da24dd24d793cdd1e86d534a0050f4997b4
u4bi <u4bi@u4bi.com>
2017-07-29 14:35:23 +0900

    split script
    - index.js

---
+++ index.js
@@ -0,0 +1,93 @@
+var renderer,
+    scene,
+    camera,
+    container;
+
+var arSource,
+    arContext,
+    arMarker;
+
+var 
+    mesh;
+
+init();
+
+function init(){
+
+
+
+    container = document.getElementById('container');
+
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    scene = new THREE.Scene();
+    camera = new THREE.Camera();
+
+    renderer.setClearColor(0x000000, 0);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    container.appendChild(renderer.domElement);
+    scene.add(camera);
+    scene.visible = false;
+
+
+    mesh = new THREE.Mesh(new THREE.BoxGeometry(2,2,2), new THREE.MeshBasicMaterial({ color : 0xFF00FF }));
+    scene.add(mesh);
+
+
+
+
+
+    arSource = new THREEx.ArToolkitSource({
+        sourceType : 'webcam',
+    });
+
+    arContext = new THREEx.ArToolkitContext({
+        cameraParametersUrl: THREEx.ArToolkitContext.baseURL + '../data/data/camera_para.dat',
+        detectionMode: 'mono',
+    });
+
+    arMarker = new THREEx.ArMarkerControls(arContext, camera, {
+        type : 'pattern',
+        patternUrl : './data/patt.hiro',
+        changeMatrixMode: 'cameraTransformMatrix'
+    });
+
+
+
+
+    /* handle */
+    arSource.init(function(){
+        arSource.onResize();
+        arSource.copySizeTo(renderer.domElement);
+
+        if(arContext.arController !== null) arSource.copySizeTo(arContext.arController.canvas);
+
+    });
+
+    arContext.init(function onCompleted(){
+        
+        camera.projectionMatrix.copy(arContext.getProjectionMatrix());
+
+    });
+
+
+    render();   
+    
+}   
+
+
+
+
+function render(){
+    requestAnimationFrame(render);
+    renderer.render(scene,camera);                
+
+    if(arSource.ready === false) return;
+
+    arContext.update(arSource.domElement);
+    scene.visible = camera.visible;
+
+
+    mesh.rotateX(.1);
+
+}          
